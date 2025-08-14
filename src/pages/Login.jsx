c45573e3589a50042bb9f6c28e9000625c4b8207
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-08-14 16:18:01 +0200

    Handle Submit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,7 +14,21 @@ const Login = () => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (!form.email || !form.password) {
+      toast.error('Veuillez remplir tous les champs !');
+      return;
+    }
+
+    if (form.password < 6) {
+      toast.error('Le mot de passe doit contenir au moins 6 caractères.');
+    };
+
+    toast.success('Connexion réussie !');
+    console.log('Formulaire envoyé : ', form);
+  };
   return (
     <DefaultLayout>
         
