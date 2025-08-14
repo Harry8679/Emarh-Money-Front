49bfaa39de70c3edfb83f23361f68491e5913f89
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-08-14 16:44:43 +0200

    Password

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -36,12 +36,28 @@ const Login = () => {
           <form onSubmit={handleSubmit} className='w-full max-w-md p-6 bg-white rounded-lg shadow-lg'>
             <h2 className="mb-4 text-2xl font-bold text-center text-blue-600">Connexion</h2>
 
+            {/* Email */}
             <div className="mb-4">
               <label htmlFor="email" className="block text-gray-700">Email</label>
               <input type="email" name='email' value={form.email} onChange={handleChange} 
                 className='w-full px-3 py-2 mt-1 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300'
                 placeholder='Entrez votre email'
               />
+            </div>
+
+            {/* Password */}
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700">Mot de passe</label>
+              <div className="relative">
+                <input type={showPassword ? 'text':'password'} name='password' value={form.password} onChange={handleChange} 
+                  className='w-full px-3 py-2 mt-1 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300'
+                  placeholder='Entrez votre mot de passe'
+                />
+                <span onClick={() => setShowPassword(!showPassword)}
+                  className='absolute text-gray-500 cursor-pointer right-3 top-3 hover:text-gray-700'>
+                    {showPassword ? "🙈" : "👁️"}
+                  </span>
+              </div>
             </div>
           </form>
         </div>
