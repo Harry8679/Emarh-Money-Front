23056bf862013d326890e61d2dbc0f90e5ab57c4
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-08-14 22:20:03 +0200

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import DefaultLayout from '../components/DefaultLayout';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const Home = () => {
   return (
     <DefaultLayout>
         <h1>This is Home Page</h1>
+        <DotLottieReact
+          src="https://lottie.host/a21018f6-3796-418a-b092-f6bff4f6ca3c/0ZyAvxJTlv.lottie"
+          loop
+          autoplay
+        />
     </DefaultLayout>
   )
 }
