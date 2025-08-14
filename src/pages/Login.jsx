28943230495d523a1f38306d0be2c470e055a0b6
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-08-14 16:33:10 +0200

    Email

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -35,6 +35,14 @@ const Login = () => {
           <ToastContainer />
           <form onSubmit={handleSubmit} className='w-full max-w-md p-6 bg-white rounded-lg shadow-lg'>
             <h2 className="mb-4 text-2xl font-bold text-center text-blue-600">Connexion</h2>
+
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700">Email</label>
+              <input type="email" name='email' value={form.email} onChange={handleChange} 
+                className='w-full px-3 py-2 mt-1 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300'
+                placeholder='Entrez votre email'
+              />
+            </div>
           </form>
         </div>
     </DefaultLayout>
